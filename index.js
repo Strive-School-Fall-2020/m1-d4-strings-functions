eb2c617d4773c41e9334e2d62f840ceaed932468
Riccardo Gulin <bazuzu666@gmail.com>
2020-10-01 13:45:28 +0200

    Added functions

--- index.js
+++ index.js
@@ -72,3 +72,93 @@ const joinedWithDots = arrayOfWords.join("............");
 console.log(joinedWithDots);
 
 console.log("HI".repeat(3));
+
+// ****************** FUNCTIONS ************************
+
+// DECLARING A FUNCTION
+// let result = 0;
+
+// const square = function (x) {
+//   console.log(typeof x);
+//   // function(parameter)
+//   // BODY OF THE FUNCTION
+
+//   result = x + x * 2;
+//   return result;
+
+//   // END OF BODY
+// };
+
+// const sum = function (a, b) {
+//   return a + b;
+// };
+
+// console.log(result);
+
+// const bark = function () {
+//   console.log("BARK!");
+// };
+
+// console.log("The result of 4*4 is: " + square(4));
+// console.log(square(6));
+
+// const squareOf25 = square("asdasd");
+
+// let myFunction = function () {
+//   console.log("Hello");
+//   console.log("Hello");
+//   console.log("Hello");
+//   console.log("Hello");
+//   console.log("Hello");
+//   console.log("Hello");
+//   console.log("Hello");
+//   console.log("Hello");
+// };
+
+// // // myFunction = function () {}; // If function is a let I can overwrite it!
+// // console.log(squareOf25);
+// myFunction();
+//console.log();
+
+// const one = function () {
+//   let a = 0;
+
+//   function two() {
+//     a = 10;
+//     return a;
+//   }
+
+//   two();
+
+//   return a;
+// };
+
+// console.log(one());
+
+// if(true){
+//     const whatever = 0
+// } else {
+
+// }
+
+// ARROW FUNCTIONS
+
+const sum = function (a, b) {
+  return a + b;
+};
+
+const sum2 = (a, b) => {
+  // NORMAL ARROW FUNCTION
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+  console.log("Hello");
+  return a + b;
+};
+
+const sum3 = (a, b) => a + b; // SHORTER SYNTAX
+const square = (x) => x * x;
